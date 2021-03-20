3c216ade03b941916b6eb2abf067f0c5c714777d
Walaa Alomari <walaahmad1987@gmail.com>
2021-03-20 15:32:08 +0200

    last solution

--- prework/let-and-const/starter-code copy/js/app.js
+++ prework/let-and-const/starter-code copy/js/app.js
@@ -1,24 +1,24 @@
 'use strict';
 
-let  names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+const names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
-let  leftImage = document.getElementById('left');
-let  centerImage = document.getElementById('center');
-let  rightImage = document.getElementById('right');
+const leftImage = document.getElementById('left');
+const centerImage = document.getElementById('center');
+const rightImage = document.getElementById('right');
 
-let  allProducts = [];
-let  container = document.getElementById('image_container');
-let  viewed = [];
-let  labels = [];
-let  pics = [leftImage, centerImage, rightImage];
-let  list = document.getElementById('productlist');
-let  totalClicks = 0;
-let  views = [];
-let  votes = [];
+let allProducts = [];
+const container = document.getElementById('image_container');
+const viewed = [];
+const labels = [];
+const pics = [leftImage, centerImage, rightImage];
+const list = document.getElementById('productlist');
+let totalClicks = 0;
+const views = [];
+const votes = [];
 
 function Product(name) {
   this.name = name;
-  this.path = 'img/' + name + '.jpg';
+  this.path = `img/ ${name}  .jpg`;
   this.votes = 0;
   this.views = 0;
   allProducts.push(this);
@@ -28,20 +28,25 @@ function makeRandom() {
   return Math.floor(Math.random() * names.length);
 }
 
+
+
+
+
+
 function displayPics(){
   while(viewed.length < 6){
-    let  rando = makeRandom();
+    const rando = makeRandom();
     while(!viewed.includes(rando)){
       viewed.push(rando);
     }
   }
-  console.log(rando);
-  // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the let iable declaration from `let  to `let`.
-  // PUT YOUR RESPONSE IN THIS COMMENT
+
+  // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the const variable declaration from `const  to `const`.
+  // PUT YOUR RESPONSE IN THIS COMMENT :because  the variable rando converted to block scoped {} so it will not be globaly defined .
   console.log(viewed);
 
-  for (let  i = 0; i < 3; i++){
-    let  temp = viewed.shift();
+  for (let i = 0; i < 3; i++){
+    const temp = viewed.shift();
     pics[i].src = allProducts[temp].path;
     pics[i].id = allProducts[temp].name;
     allProducts[temp].views += 1;
@@ -59,10 +64,10 @@ function handleClick(event) {
     showList();
     makeChart();
   }
-  for(let  i = 0; i < names.length; i++){
+  for(let i = 0; i < names.length; i++){
     if(event.target.id === allProducts[i].name) {
       allProducts[i].votes += 1;
-      console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
+      console.log(`${event.target.id}   has  ${allProducts[i].votes}    votes in  ${allProducts[i].views}  views`);
     }
   }
   localStorage.busmall = JSON.stringify(allProducts);
@@ -71,9 +76,9 @@ function handleClick(event) {
 }
 
 function showList() {
-  for(let  i = 0; i < allProducts.length; i++) {
-    let  liEl = document.createElement('li');
-    liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
+  for(let i = 0; i < allProducts.length; i++) {
+    const liEl = document.createElement('li');
+    liEl.textContent = `${allProducts[i].name}  has ${allProducts[i].votes}   votes in ${allProducts[i].views }  views`;
     list.appendChild(liEl);
   }
 }
@@ -88,7 +93,7 @@ function makeChartData(){
 
 function makeChart(){
   makeChartData();
-  let  ctx = document.getElementById('chartypants').getContext('2d');
+  const ctx = document.getElementById('chartypants').getContext('2d');
   new Chart(ctx, { //eslint-disable-line
     type: 'bar',
     data: {
@@ -128,7 +133,7 @@ if(localStorage.busmall){
   allProducts = JSON.parse(localStorage.busmall);
 } else {
   console.log('There is no local storage data; initialize app by creating instances');
-  for(let  i = 0; i < names.length; i++) {
+  for(let i = 0; i < names.length; i++) {
     new Product(names[i]);
   }
 }
